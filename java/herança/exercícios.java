834030c1a3ddb8d54f623e959d3cf33ae0d2eee2
Lorena Ricoy <104567219+lorenaricoy@users.noreply.github.com>
2022-11-12 12:21:04 -0300

    Update exercícios.java

--- java/herança/exercícios.java
+++ java/herança/exercícios.java
@@ -1,3 +1,7 @@
+/*1. Vamos criar uma classe Conta, que possua um saldo, os métodos para pegar saldo,
+depositar e sacar.
+2. Adicione um método na classe Conta, que atualiza essa conta de acordo com uma taxa
+percentual fornecida.*/
 //Classe Conta
 
 class Conta {
@@ -24,6 +28,11 @@ class Conta {
     }
 }
 
+/*3. Crie duas subclasses da classe Conta: ContaCorrente e ContaPoupanca. Ambas terão o
+método atualiza reescrito: A ContaCorrente deve atualizar-se com o dobro da taxa e a
+ContaPoupanca deve atualizar-se com o triplo da taxa. Além disso, a ContaCorrente deve
+reescrever o método deposita, a fim de retirar uma taxa bancária de dez centavos de cada
+depósito.*/
 //Classe herança ContaPoupança1
 
 public class ContaPoupança1 extends Conta {
@@ -50,6 +59,43 @@ public class ContaCorrente1 extends Conta {
     }
 }
 
+/*5. Vamos criar uma classe que seja responsável por fazer a atualização de todas as contas
+bancárias e gerar um relatório com o saldo anterior e saldo novo de cada uma das contas.*/
+//Classe AtualizaContas
+
+public class AtualizaContas {
+    private double saldoTotal;
+    private double selic;
+    
+    public void setSaldoTotal(double saltoTotal) {
+        this.saldoTotal = saltoTotal;
+    }
+
+    public double getSaldoTotal() {
+        return saldoTotal;
+    }
+
+    public AtualizaContas (double selic){
+        this.selic = selic;
+    }
+
+    public void roda (Conta c){
+        System.out.println("saldo anterior: " + c.saldo);
+        c.atualiza(selic);
+        System.out.println("saldo atual: " + c.saldo);
+
+        saldoTotal =+ c.saldo;
+    }
+    Banco b;
+}
+
+/*6. Crie uma classe Banco que possui um array de Conta. Repare que num array de Conta
+você pode colocar tanto ContaCorrente quanto ContaPoupanca. Crie um método public
+void adiciona(Conta c), um método public Conta pegaConta(int x) e outro public int
+pegaTotalDeContas(), muito similar a relação anterior de Empresa-Funcionario.
+Faça com que seu método main crie diversas contas, insira-as no Banco e depois, com um
+for, percorra todas as contas do Banco para passá-las como argumento para o
+AtualizadorDeContas.*/
 //Classe Banco
 
 class Banco {
@@ -82,34 +128,6 @@ class Banco {
     public int pegaTotalDeContas(){
         return this.numContas;
     }
-}
-
-//Classe AtualizaContas
-
-public class AtualizaContas {
-    private double saldoTotal;
-    private double selic;
-    
-    public void setSaldoTotal(double saltoTotal) {
-        this.saldoTotal = saltoTotal;
-    }
-
-    public double getSaldoTotal() {
-        return saldoTotal;
-    }
-
-    public AtualizaContas (double selic){
-        this.selic = selic;
-    }
-
-    public void roda (Conta c){
-        System.out.println("saldo anterior: " + c.saldo);
-        c.atualiza(selic);
-        System.out.println("saldo atual: " + c.saldo);
-
-        saldoTotal =+ c.saldo;
-    }
-    Banco b;
 }
 
 //Classe main para teste
